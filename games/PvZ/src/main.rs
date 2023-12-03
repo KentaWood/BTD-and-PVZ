7fa43dbfaf65872b66acc335f5dd4e69d076d2f1
Woodale002 <Umehara.kentaro@gmail.com>
2023-12-03 12:44:55 -0800

    updatesd un organized but pea shooting and walking are done

--- games/PvZ/src/main.rs
+++ games/PvZ/src/main.rs
@@ -1,78 +1,23 @@
 use engine_immediate as engine;
 use engine_immediate::{geom::*, Camera, Engine, SheetRegion};
 use rand::Rng;
+mod util;
+use util::screen_to_grid;
+use util::convert_mouse_pos;
+
 
 const W: f32 = 1400.0;
 const H: f32 = 600.0;
 
 const SPRITE_PLANT_PEASHOOTER: SheetRegion = SheetRegion::new(0, 448 , 128, 0, 64, 64);
-const SPRITE_ZOMBIE_NORMAL: SheetRegion = SheetRegion::new(0, 0, 0, 0, 64, 128);
-
 const PLANT_SIZE_PEASHOOTER: Vec2 = Vec2 { x: 64.0, y: 64.0 };
+
+const SPRITE_ZOMBIE_NORMAL: SheetRegion = SheetRegion::new(0, 0, 0, 0, 64, 128);
 const ZOMBIE_SIZE_NORMAL: Vec2 = Vec2 { x: 64.0, y: 128.0 };
 
-fn screen_to_grid(x: f32, y: f32) -> (f32, f32) {
-    let mut grid_x = 700.0; // Default values
-    let mut grid_y = 300.0;
+const SPRITE_PEA: SheetRegion = SheetRegion::new(0, 66, 36, 0, 10, 16);
+const PEA_SIZE: Vec2 = Vec2 { x: 24.0, y: 32.0 };
 
-    if x > 240.0 && x < 340.0 {
-        grid_x = 280.0;
-    }
-
-    if x > 340.0 && x < 410.0{
-        grid_x = 380.0;
-    }
-
-    if x > 410.0 && x < 500.0{
-        grid_x = 450.0;
-    }
-
-    if x > 500.0 && x < 575.0{
-        grid_x = 540.0;
-    }
-
-    if x > 575.0 && x < 660.0{
-        grid_x = 620.0;
-    }
-
-    if x > 660.0 && x < 730.0{
-        grid_x = 700.0;
-    }
-
-    if x > 730.0 && x < 815.0{
-        grid_x = 780.0;
-    }
-
-    if x > 815.0 && x < 900.0{
-        grid_x = 850.0;
-    }
-
-    if x > 900.0 && x < 1000.0{
-        grid_x = 940.0;
-    }
-    //y- cord
-    if y > 30.0 && y < 125.0 {
-        grid_y = 70.0;
-    }
-
-    if y > 125.0 && y < 220.0{
-        grid_y = 175.0;
-    }
-
-    if y > 220.0 && y < 320.0{
-        grid_y = 265.0;
-    }
-
-    if y > 320.0 && y < 415.0{
-        grid_y = 370.0;
-    }
-
-    if y > 415.0 && y < 535.0{
-        grid_y = 470.0;
-    }
-
-    (grid_x, grid_y)
-}
 
 fn empty_Space(plants: &Vec<Plant>, x: f32, y: f32) -> bool {
     for plant in plants {
@@ -84,9 +29,13 @@ fn empty_Space(plants: &Vec<Plant>, x: f32, y: f32) -> bool {
 }
 
 
-
 struct Plant {
     pos: Vec2,
+}
+
+struct Pea {
+    pos: Vec2,
+    vel: Vec2,
 }
 
 struct Zombie {
@@ -100,11 +49,17 @@ struct Game {
     font: engine::BitFont,
     spritesheet: engine::Spritesheet,
     spritesheet2: engine::Spritesheet,
+    spritesheet3: engine::Spritesheet,
     plants: Vec<Plant>,
     zombies: Vec<Zombie>,
+    peas: Vec<Pea>,
+    zombie_count :usize,
+    plant_count: usize,
+    pea_count: usize,
     mode: u32, // 0 = PvZ, 1 = Game Won, 2 = Game Over 
     mouse_clicked: bool,
     plant_index: usize,
+    once: bool,
 }
 
 impl engine::Game for Game {
@@ -121,6 +76,9 @@ impl engine::Game for Game {
         let sprite_img2 = image::open("/Users/nobuko/Desktop/CS- 181G Projects/unit-3-Alex /Unit-3/assets/plantsZombies/icon.png").unwrap().into_rgba8();
         let spritesheet2 = engine.add_spritesheet(sprite_img2, Some("character spritesheet"));
 
+        let sprite_img3 = image::open("/Users/nobuko/Desktop/CS- 181G Projects/Unit-3/assets/plantsZombies/PeashooterAssetsDS.webp").unwrap().into_rgba8();
+        let spritesheet3 = engine.add_spritesheet(sprite_img3, Some("character spritesheet"));
+
         let font = engine::BitFont::with_sheet_region(
             '0'..='9',
             SheetRegion::new(0, 0, 512, 0, 80, 8),
@@ -131,13 +89,19 @@ impl engine::Game for Game {
         Game {
             spritesheet,
             spritesheet2,
+            spritesheet3,
             score: 0,
             font,
             mode: 0,
             plants: Vec::with_capacity(16),
-            zombies:Vec::with_capacity(16),
+            zombies: Vec::with_capacity(16),
+            peas: Vec::with_capacity(16),
             mouse_clicked: false,
             plant_index: 0,
+            zombie_count: 0,
+            plant_count: 0,
+            pea_count: 0,
+            once: true,
             
         }
 
@@ -145,13 +109,78 @@ impl engine::Game for Game {
     }
     fn update(&mut self, engine: &mut Engine) {
 
+        // if self.once {
+        //     // pea shooting of the plants 
+        //     self.plants.push(Plant {
+        //         pos: Vec2 {
+        //             x: 280.0,
+        //             y: 70.0,
+        //         },
+        //     });
+        
+        //     self.peas.push(Pea {
+        //         pos: Vec2 {
+        //             x: 280.0,
+        //             y: 70.0,
+        //         },
+        //         vel: Vec2 {
+        //             x: 4.0,
+        //             y: 0.0,
+        //         },
+        //     });
+        
+        //     self.plant_count = 1;
+        //     self.pea_count = 1;
+        
+        //     for pea in self.peas.iter_mut() {
+        //         pea.pos.x += pea.vel.x;
+        //     }
+        
+        //     self.once = false;
+        // }
+
+        // for pea in self.peas.iter_mut() {
+        //     pea.pos.x += pea.vel.x;
+        // }
+        
+        //moving of the zombies
+
+        if self.once{
+
+            self.zombies.push(Zombie {
+                pos: Vec2 {
+                    x: 1100.0,
+                    y: 90.0,
+                },
+                vel: Vec2 {
+                    x: -0.25,
+                    y: 0.0,
+                },
+            });
+            
+
+
+            self.once = false;
+
+        }
+
+        
+        for zombie in self.zombies.iter_mut() {
+            zombie.pos.x += zombie.vel.x;
+        }
+        
+        
+
+
+
+
+
+
+        //Handles the placement of plants
         if self.mouse_clicked {
-
-
             if engine.input.is_mouse_down(winit::event::MouseButton::Left) {
                 let mouse_pos = engine.input.mouse_pos();
-                let mouse_x =  mouse_pos.x as f32 / 1.14285714;
-                let mouse_y =  (1200.0 - mouse_pos.y as f32) / 2.0;
+                let (mouse_x, mouse_y) = convert_mouse_pos(mouse_pos.into());
 
                 // println!("{:?}", mouse_pos);
                 println!("{}, {}", mouse_x, mouse_y);
@@ -163,10 +192,9 @@ impl engine::Game for Game {
             }
 
             else{
-                let mouse_pos = engine.input.mouse_pos();
-                let mouse_x =  mouse_pos.x as f32 / 1.14285714;
-                let mouse_y =  (1200.0 - mouse_pos.y as f32) / 2.0;
 
+                let mouse_pos = engine.input.mouse_pos();
+                let (mouse_x, mouse_y) = convert_mouse_pos(mouse_pos.into());
                 let (grid_x, grid_y) = screen_to_grid(mouse_x, mouse_y);
 
                 if empty_Space(&self.plants, grid_x, grid_y) {
@@ -191,8 +219,7 @@ impl engine::Game for Game {
         else{
             if engine.input.is_mouse_down(winit::event::MouseButton::Left) {
                 let mouse_pos = engine.input.mouse_pos();
-                let mouse_x =  mouse_pos.x as f32 / 1.14285714;
-                let mouse_y =  (1200.0 - mouse_pos.y as f32) / 2.0;
+                let (mouse_x, mouse_y) = convert_mouse_pos(mouse_pos.into());
 
                 self.mouse_clicked = true;
                 self.plants.push(Plant {
@@ -202,7 +229,7 @@ impl engine::Game for Game {
                     },
                 });
                 }
-            }
+        }
 
 
 
@@ -221,33 +248,32 @@ impl engine::Game for Game {
             SheetRegion::new(0, 0, 0, 16, 1400, 600),
         );
 
-        //draw Zombie 
-        engine.draw_sprite(
-            self.spritesheet2,
-            AABB{
-                center: Vec2 {
-                    x: 1100.0 ,
-                    y: 90.0 ,
-                },
-                size: ZOMBIE_SIZE_NORMAL,
-            },
-            
-            SPRITE_ZOMBIE_NORMAL,
-        );
-
+        // //draw Zombie 
         // engine.draw_sprite(
         //     self.spritesheet2,
         //     AABB{
         //         center: Vec2 {
-        //             x: 280.0 ,
-        //             y: 200.0 ,
+        //             x: 1100.0 ,
+        //             y: 90.0 ,
         //         },
-        //         size: PLANT_SIZE_PEASHOOTER,
+        //         size: ZOMBIE_SIZE_NORMAL,
         //     },
             
-        //     SPRITE_PLANT_PEASHOOTER,
+        //     SPRITE_ZOMBIE_NORMAL,
         // );
-       
+
+        engine.draw_sprite(
+            self.spritesheet2,
+            AABB{
+                center: Vec2 {
+                    x: 280.0 ,
+                    y: 70.0 ,
+                },
+                size: PLANT_SIZE_PEASHOOTER,
+            },
+            
+            SPRITE_PLANT_PEASHOOTER,
+        );
 
         for plant in self.plants.iter() {
             engine.draw_sprite(
@@ -259,6 +285,30 @@ impl engine::Game for Game {
                 SPRITE_PLANT_PEASHOOTER,
             );
         }
+
+        for zombie in self.zombies.iter() {
+            engine.draw_sprite(
+                self.spritesheet2,
+                AABB {
+                    center: zombie.pos,
+                    size: ZOMBIE_SIZE_NORMAL,
+                },
+                SPRITE_ZOMBIE_NORMAL,
+            );
+        }
+
+        for pea in self.peas.iter() {
+            engine.draw_sprite(
+                self.spritesheet3,
+                AABB {
+                    center: pea.pos,
+                    size: PEA_SIZE,
+                },   
+                SPRITE_PEA,
+            );
+        }
+
+        
             
     }
 }
